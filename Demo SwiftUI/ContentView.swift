cbe01e0934a0e08bf68370fc5e18c7342fa38fef
Maxime Girard <maxime.girard.cpro@gmail.com>
2023-10-09 16:16:45 +0200

    Initial Commit

---
+++ Demo SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Demo SwiftUI
+//
+//  Created by Maxime Girard on 09/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Demo SwiftUI/Demo_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_SwiftUIApp.swift
+//  Demo SwiftUI
+//
+//  Created by Maxime Girard on 09/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Demo_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
